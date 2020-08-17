0481d63e22ab3e4d1f374fe3d911c67baf2bbba7
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-08-17 17:12:35 +1000

    ✨ UPDATED: Added navbar for mobile view

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Me from './BodyItems/Me';
-import Skills from './BodyItems/Skills';
-import Education from './BodyItems/Education';
-import Projects from './BodyItems/Projects';
-import Footer from './Footer';
+import React from "react";
+import Me from "./BodyItems/Me";
+import Skills from "./BodyItems/Skills";
+import Education from "./BodyItems/Education";
+import Projects from "./BodyItems/Projects";
+import Footer from "./Footer";
 
 const Body = () => {
-	return (
-		<div className="body-container">
-			<div className="container">
-				<Me/>
-				<Skills/>
-				<Projects/>
-				<Education/>
-				<Footer/>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="body-container">
+      <div className="container">
+        <Me />
+        <Projects />
+        <Skills />
+        <Education />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,103 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
-	return (
-		<>
-			<header>
-                <Link
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration= {1000}
-                >Home</Link>
-                <Link
-                    to="me"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration= {1000}
-                >Me</Link>
-                <Link
-                    to="skills"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration= {1000}
-                >Skills</Link>
-                <Link
-                    to="projects"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration= {1000}
-                >Projects</Link>
-                <Link
-                    to="education"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration= {1000}
-                >Education</Link>
-            </header>
-		</>
-	)
-}
+  const [open, setOpen] = useState(false);
+  const handleNavOpen = () => {
+    let hamburger = document.querySelector("nav .hamburger-btn");
+    let navBar = document.querySelector("nav .nav-bar");
+    let blurBackground = document.querySelector("nav .blur-background");
+    if (open) {
+      setOpen(!open);
+      hamburger.classList.remove("open");
+      navBar.classList.remove("open");
+      blurBackground.classList.remove("open");
+    } else {
+      setOpen(!open);
+      hamburger.classList.add("open");
+      navBar.classList.add("open");
+      blurBackground.classList.add("open");
+    }
+  };
+  return (
+    <>
+      <header>
+        <Link to="home" spy={true} smooth={true} offset={0} duration={1000}>
+          Home
+        </Link>
+        <Link to="me" spy={true} smooth={true} offset={-70} duration={1000}>
+          Me
+        </Link>
+        <Link
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={1000}
+        >
+          Projects
+        </Link>
+        <Link to="skills" spy={true} smooth={true} offset={-70} duration={1000}>
+          Skills
+        </Link>
+        <Link
+          to="education"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={1000}
+        >
+          Education
+        </Link>
+      </header>
+      {/* On Mobile */}
+      <nav>
+        <div className="blur-background"></div>
+        <div className="hamburger-btn" onClick={handleNavOpen}>
+          <div className="hamburger"></div>
+        </div>
+        <div className="nav-bar">
+          <div className="icon" onClick={handleNavOpen}>
+            <IoMdClose className="icon-close" />
+          </div>
+          <Link to="home" spy={true} smooth={true} offset={0} duration={1000}>
+            Home
+          </Link>
+          <Link to="me" spy={true} smooth={true} offset={-70} duration={1000}>
+            Me
+          </Link>
+          <Link
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={1000}
+          >
+            Projects
+          </Link>
+          <Link
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={1000}
+          >
+            Skills
+          </Link>
+          <Link
+            to="education"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={1000}
+          >
+            Education
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Header;
