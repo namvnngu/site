54c3a26a6e6e5e25ed2381499d84ac436e6f62fd
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-02-14 20:56:07 +1100

    🐛 Fixed next/img bug where it could not be used in next export

--- components/MainImage/index.tsx
+++ components/MainImage/index.tsx
@@ -1,16 +1,31 @@
-import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import styles from "../../styles/Home.module.scss";
 
 const MainImage = () => {
+  const [blur, setBlur] = useState(true);
+  const loadingImage = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (loadingImage.current?.complete) {
+      setBlur(false);
+    }
+    
+    loadingImage.current?.addEventListener('load', () => {
+      setBlur(false);
+    });
+  }, []);
+
   return (
     <>
       <div style={{ position: "relative", height: "600px" }}>
-        <Image
+      <div className={styles["image-container"]}>
+        <img
+        className={blur ? styles["blur"] : styles["unblur"]}
           src="/images/background/main-desktop.gif"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="85%"
           alt="Pixel art with a moon and a big yellow in jungle made by Name Nguyen"
+          ref={loadingImage}
         />
+      </div>
       </div>
     </>
   );
