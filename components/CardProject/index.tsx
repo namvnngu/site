566448cf84a5086ab0ebe0a13575afabb48cd106
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-02-16 16:28:51 +1100

    ✨ Created and styled card

---
+++ components/CardProject/index.tsx
@@ -0,0 +1,35 @@
+import styles from "../../styles/CardProject.module.scss";
+import Link from "next/link";
+
+export interface CardProjectProps {
+  image: string;
+  title: string;
+  description: string;
+  alt: string;
+  link: string;
+}
+
+const CardProject: React.FC<CardProjectProps> = ({
+  image,
+  title,
+  description,
+  alt,
+  link,
+}) => {
+  return (
+    <Link href={link}>
+      <a>
+        <div className={styles["card"]}>
+          <span className="fill"></span>
+          <section>
+            <img src={image} alt={alt} />
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </section>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default CardProject;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import styles from "../styles/Home.module.scss";
 import Head from "next/head";
 import MainImage from "../components/MainImage";
 import Button from "../components/Button";
+import Link from "next/link";
+import CardProject from "../components/CardProject";
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
         <title>Home | Name Nguyen</title>
       </Head>
       <div className={styles["home"]}>
+        {/* Main Image */}
         <div className={styles["main-image-container"]}>
           <MainImage />
           <div className={styles["titles"]}>
@@ -23,6 +26,65 @@ export default function Home() {
             <Button text={"Projects →"} url={"/projects"} />
           </div>
         </div>
+
+        {/* Introduction */}
+        <section className="container">
+          <div className={styles["introduction"]}>
+            <p>
+              I am a third-year undergraduate student at Deakin University,
+              Melbourne. I think that I am a <span>funny guy</span> who loves
+              building <span>programs</span> that makes{" "}
+              <span>people's lives easier</span>. At the moment, I specialize in
+              web development, and I am trying different areas such as{" "}
+              <span>computer graphics</span> and <span>game development</span>{" "}
+              by myself.
+            </p>
+            <div>
+              <p>
+                In my spare time, you can catch me filming, drawing pixel art,
+                reading books and learning English
+              </p>
+              <p>
+                Take a look on my{" "}
+                <span>
+                  <Link href="/projects">
+                    <a>projects</a>
+                  </Link>
+                </span>{" "}
+                page or{" "}
+                <span>
+                  <Link href="/resume">
+                    <a>resume</a>
+                  </Link>
+                </span>{" "}
+                to understand about me a bit better, or visit contact page to
+                reach me out and say "G'day". I am seeking for a developer job
+                and open to hearing about new opportunities.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        {/* Featured Projects */}
+        <section className={styles["featured-projects"]}>
+          <h1>Featured Projects</h1>
+          <section>
+            <CardProject
+              image="/images/projects/ecosystem/ecosystem-thumb.png"
+              title="Ecosystem Simulation"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..."
+              alt="ecosystem thumbnail"
+              link="/projects/ecosystem-simulation"
+            />
+            <CardProject
+              image="/images/projects/ecosystem/ecosystem-thumb.png"
+              title="Ecosystem Simulation"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..."
+              alt="ecosystem thumbnail"
+              link="/projects/ecosystem-simulation"
+            />
+          </section>
+        </section>
       </div>
     </>
   );
